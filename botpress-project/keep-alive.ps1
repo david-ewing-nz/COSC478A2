9b56bc15e3fb7120f285aaa0f99017f39f2197b1
david.ewing.nz <david.ewing.nz@icloud.com>
2024-10-31 08:19:25 +1300

    update changes to bot before starting after discussion with Prof Trevor

---
+++ botpress-project/keep-alive.ps1
@@ -0,0 +1,2 @@
+$env:GOOGLE_API_KEY="AIzaSyA9QBxdGeyayIOfG_0AeVM6Zvo7avRbW_c"
+while ($true) { Invoke-WebRequest -Uri "https://d07e-2407-7000-98b5-2c00-5c00-c181-de53-a950.ngrok-free.app" -UseBasicParsing; Start-Sleep -Seconds 300 }
